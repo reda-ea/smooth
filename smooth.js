a0a6a15c84432db5efed63103bbb3ddb34f17904
Reda El Khattabi <reda.ea@gmail.com>
2014-08-10 08:20:21 +0000

    basic template rendering

--- smooth.js
+++ smooth.js
@@ -75,10 +75,113 @@ window.Smooth = (function() {
         return element;
     }
 
+    // data is an object, context is an array. always.
+    function resolveContext(data, context) {
+        if(context.length === 0) return data;
+        var newdata = data;
+        switch(typeof context[0]) {
+            case 'string':
+                newdata = newdata[context[0]];
+                break;
+            case 'object':
+                if(context[0].index) newdata = newdata[context[0].index];
+                if(context[0].key) newdata = newdata[context[0].key];
+                break;
+            default:
+                throw new Error('Invalid context : ' + context[0]);
+        }
+        // if(context.length === 1) return newdata; // last value uninterpreted
+        if(typeof(newdata) == 'function')
+            newdata = newdata.call(data);
+        return resolveContext(newdata, context.slice(1));
+    }
+
+    function getAccessor(data, context, key) {
+        var accessor = {};
+        var current = resolveContext(data, context);
+        var valtype = typeof(current[key]);
+        // define get/set & resolve funcs actual type
+        switch(valtype) {
+            case 'function':
+                accessor.get = function() {
+                    return resolveContext(data, context)[key]();
+                };
+                accessor.set = function(value) {
+                    resolveContext(data, context)[key](value);
+                };
+                valtype = typeof(current[key]());
+                break;
+            case 'boolean':
+            case 'number':
+            case 'string':
+            case 'object':
+                accessor.get = function() {
+                    return resolveContext(data, context)[key];
+                };
+                accessor.set = function(value) {
+                    resolveContext(data, context)[key] = value;
+                };
+                break;
+            case 'undefined':
+                break;
+            default:
+                throw new Error('value type not handled: ' + typeof(data[key]));
+        }
+        // accessor type
+        switch(valtype) {
+            case 'boolean':
+            case 'number':
+            case 'string':
+                accessor.type = 'value';
+                break;
+            case 'object':
+                // TODO arrays
+                accessor.type = 'object';
+                break;
+            case 'undefined':
+                accessor.type = 'undefined';
+                break;
+            case 'function':
+                throw new Error('function resolved to another function');
+        }
+        return accessor;
+    }
+
+    // TODO handle form elements
+    // TODO also set watches on change etc
+    function applyValue(element, target, accessor) {
+        if(accessor.type == 'value') {
+            var value = accessor.get();
+            if(value == null) value = '';
+            if(!target)
+                element.innerHTML = value;
+            else
+                element.setAttribute(target, value);
+        }
+        // TODO duplicate array children here
+    }
+
+    function applyObject(element, data) {
+        // step 1 - only main bindings
+        if(!element.__smooth) return;
+        for(var i = 0; i < element.__smooth.bindings.length; i++) {
+            var binding = element.__smooth.bindings[i];
+            if(binding.type == 'data')
+                applyValue(element, binding.target,
+                    getAccessor(data, element.__smooth.context, binding.value));
+            // TODO handle actions
+        }
+        for(var i = 0; i < element.__smooth.descendants.length; i++)
+            applyObject(element.__smooth.descendants[i], data);
+    }
+
     return {
         attribute: 'data-bind',
         analyze: function(element) {
             return analyzeDom(element, [], this.attribute);
+        },
+        render: function(element, data) {
+            applyObject(analyzeDom(element, [], this.attribute), data);
         },
         version: 0.1
     };
