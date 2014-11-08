c484d79c83c40776e680ed555eeb61785dd06757
Reda El Khattabi <reda.ea@gmail.com>
2014-11-08 04:01:15 +0000

    json loading

--- bossa.js
+++ bossa.js
@@ -5,11 +5,11 @@
         var head = document.getElementsByTagName('head')[0];
         var script = document.createElement('script');
         var failtout = setTimeout(function() {
-            fail();
+            fail && fail();
         }, 15000);
         var successcb = function() {
             clearTimeout(failtout);
-            success();
+            success && success();
         };
         script.type = 'text/javascript';
         script.src = url;
@@ -34,7 +34,7 @@
                 if (link[sheet] && link[sheet][cssRules].length) {
                     clearInterval(interval_id);
                     clearTimeout(timeout_id);
-                    success();
+                    success && success();
                 }
             }catch(e){console.error(e);}
         }, 10);
@@ -56,7 +56,7 @@
                     template.controller.call(template.container);
                 } catch(e) {
                     console.error(e);
-                    fail(e);
+                    fail && fail(e);
                     return;
                 }
             return;
@@ -71,15 +71,56 @@
                         template.controller.call(template.container);
                     } catch(e) {
                         console.error(e);
-                        fail(e);
+                        fail && fail(e);
                         return;
                     }
-                success();
+                success && success();
             } else
-                fail();
+                fail && fail();
         };
-        xhr.open("GET", template.url, true);
+        xhr.open('GET', template.url, true);
         xhr.send();
+    }
+    
+    function sameOrigin(url) {
+        var loc = window.location,
+            a = document.createElement('a')
+        a.href = url
+
+        return a.hostname === loc.hostname &&
+            a.port === loc.port &&
+            a.protocol === loc.protocol &&
+            loc.protocol !== 'file:'
+    }
+    
+    var loadOneJSON = function(url, success, fail) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) { // `DONE`
+                var status = xhr.status;
+                if (status == 200) {
+                    var data = JSON.parse(xhr.responseText);
+                    success && success.call(data, data);
+                }
+                else {
+                    fail && fail(new Error(status));
+                }
+            }
+        };
+        xhr.open('GET', url, true);
+        xhr.send();
+    };
+    
+    function loadOneJSONP(url, success, fail) { // url should contain $JSONP
+        // new callback name
+        var r4 = function(){
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16).substring(1);
+        };
+        var cbname = 'bossa' + r4() + r4() + r4() + r4();
+        window[cbname] = success;
+        var fullurl = url.replace('$JSONP', cbname);
+        return loadOneScript(fullurl, null, fail);
     }
     
     // TODO add json/jsonp
@@ -125,6 +166,27 @@
             }, function() {
                 if(typeof(fail)=='function')
                     fail.apply([].slice.call(ctn.children), arguments);
+            });
+        },
+        json: function(url, success, fail) {
+            // alternate syntax: url, alias, success, fail (caches result)
+            if(typeof(success) == 'string') {
+                var alias = success;
+                success = fail;
+                if(arguments.length > 3)
+                    fail = arguments[3];
+            }
+            if(!url) throw Error('Content URL must be provided');
+            var func = (url.indexOf('$JSONP') == -1) ?
+                        loadOneJSON : loadOneJSONP;
+            func(url, function() {
+                if(typeof(success)=='function') {
+                    if(alias) window[alias] = arguments[0]
+                    success.apply(arguments[0], arguments);
+                }
+            }, function() {
+                if(typeof(fail)=='function')
+                    fail.apply(null, arguments);
             });
         }
     }
